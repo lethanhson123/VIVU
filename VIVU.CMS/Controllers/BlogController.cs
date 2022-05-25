1d1fa1993a06afaad471aafc39b644bda673c222
Le Thanh Son <ltson@theodorealexander.com>
2022-05-25 21:35:09 +0700

    20220525

---
+++ VIVU.CMS/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VIVU.CMS.Models;
+
+namespace VIVU.CMS.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int Id)
+        {
+            return View();
+        }
+    }
+}
---
+++ VIVU.CMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VIVU.CMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
