e69eacc1d00a5d8ad9435fad3cbcfd3713a8e127
Dương Nguyễn Tấn Hòa <tanhoatm@gmail.com>
2022-05-31 13:18:58 +0700

    Update view

---
+++ VIVU.CMS/Controllers/MarketLeadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VIVU.CMS.Controllers
+{
+    public class MarketLeadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VIVU.CMS/Controllers/TagController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VIVU.CMS.Controllers
+{
+    public class TagController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
