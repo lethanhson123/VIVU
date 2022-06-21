7531c314b9ae4bfe0955cd9490d75f6dc47976a5
Dinh Sinh Nguyen <nguyendinhsinh95@gmail.com>
2022-06-21 22:30:42 +0700

    f

--- VIVU.Logic/Configs/AuthenticateConfig.cs
+++ VIVU.Logic/Configs/AuthenticateConfig.cs
@@ -13,7 +13,6 @@ namespace VIVU.Logic.Configs
         public string SecretKey { get; set; } = string.Empty;
         public int TokenExpireAfterMinutes { get; set; } = 0;
         public string DefaultRedirect { get; set; } = string.Empty;
-        public string SecretKey { get; set; } =string.Empty;
         public string VerifySignUp { get; set; } = string.Empty;
     }
 }
