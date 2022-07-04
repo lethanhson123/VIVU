2c1fa861a2d8dec0fb82e995de170a8d0766c9c2
DNTHoa <tanhoatm@gmail.com>
2022-07-04 22:03:59 +0700

    update code

--- VIVU.Website/Controllers/HomeController.cs
+++ VIVU.Website/Controllers/HomeController.cs
@@ -23,16 +23,6 @@ namespace VIVU.Website.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
